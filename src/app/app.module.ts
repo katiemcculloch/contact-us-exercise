caf71d2cf3d186af4efd6d9f9683ca84cca92a71
Katie McCulloch <katiee_mcculloch@yahoo.com>
2018-11-17 15:43:02 -0800

    adds form modules and posts form inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { ContactComponent } from "./contact/contact.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { Routes, RouterModule } from "@angular/router";
 
@@ -20,7 +22,13 @@ const appRoutes: Routes = [
     HomeComponent,
     ContactComponent
   ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
